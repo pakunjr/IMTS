b11cb7fc7a7512239c02248f2abfb3928f43dd0a
pakunjr <pakunjr@gmail.com>
2014-10-10 15:09:05 +0800

    Updates
    web.config is added for IIS 7 servers.

--- application/config.php
+++ application/config.php
@@ -7,17 +7,30 @@ $configs['SYSTEM_AUTHOR'] = 'PakunJr';
 $configs['SYSTEM_TEMPLATE'] = 'default';
 $configs['SYSTEM_YEAR_START'] = '2014';
 
-$configs['URL_BASE'] = 'http://'.$_SERVER['HTTP_HOST'].'/IMTS/';
+if ($_SERVER['HTTP_HOST'] == 'localhost')
+    $configs['URL_BASE'] = 'http://'.$_SERVER['HTTP_HOST'].'/IMTS/';
+else
+    $configs['URL_BASE'] = 'http://'.$_SERVER['HTTP_HOST'].'/';
 $configs['URL_TEMPLATE'] = $configs['URL_BASE'].'public/templates/'.$configs['SYSTEM_TEMPLATE'].'/';
 
 // Default configuration of the database
 // which is the IMTS database server
-$configs['DATABASE_HOST'] = '127.0.0.1';
-$configs['DATABASE_USERNAME'] = 'root';
-$configs['DATABASE_PASSWORD'] = 'sysdev09';
-$configs['DATABASE_NAME'] = 'db_imts';
-$configs['DATABASE_PORT'] = '3306';
-$configs['DATABASE_SOCKET'] = '';
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $configs['DATABASE_HOST'] = '127.0.0.1';
+    $configs['DATABASE_USERNAME'] = 'root';
+    $configs['DATABASE_PASSWORD'] = 'sysdev09';
+    $configs['DATABASE_NAME'] = 'db_imts';
+    $configs['DATABASE_PORT'] = '3306';
+    $configs['DATABASE_SOCKET'] = '';
+} else {
+    // This config is the default for the server
+    $configs['DATABASE_HOST'] = '127.0.0.1';
+    $configs['DATABASE_USERNAME'] = 'eqmtsdb';
+    $configs['DATABASE_PASSWORD'] = 'UYT436DSFGFlqywp';
+    $configs['DATABASE_NAME'] = 'eqmts';
+    $configs['DATABASE_PORT'] = '3306';
+    $configs['DATABASE_SOCKET'] = '';
+}
 
 // Other database server configurations
 $databaseConfigs = array(
--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<!DOCTYPE html><html><head>
-<title>Fatal Error</title>
-</head><body>
-<div style="color: #f00;">Error: Sorry, you cannot access this page directly.</div>
-</body></html>
+<?php
+
+$file = dirname(__FILE__).DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR.'index.php';
+if (file_exists($file)) {
+    require_once($file);
+    exit();
+} else {
+    echo '<!DOCTYPE html><html><head>
+        <title>Fatal Error</title>
+        </head><body>
+        <div style="color: #f00;">
+            Error: Sorry, something went wrong with the system.<br />
+            Please send us an email if you see this message.<br />
+            Thank you.<br />
+            <br />
+            <a href="mailto:sysdev@lorma.edu">Email us at sysdev@lorma.edu, just click here.</a>
+        </div>
+        </body></html>';
+}
