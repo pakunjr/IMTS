9fce268c72b14c7ffd87e0372dd4becf1945cf85
pakunjr <pakunjr@gmail.com>
2014-10-31 11:34:51 +0800

    Updates
    Updates

---
+++ application/app_authentication/models/pages.php
@@ -0,0 +1,18 @@
+<?php
+
+class ModelPages extends Database
+{
+
+    public function __construct ()
+    {
+        parent::__construct();
+    }
+
+
+
+    public function __destruct ()
+    {
+
+    }
+
+}
